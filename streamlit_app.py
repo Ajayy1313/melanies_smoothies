f4ec4e6a1db97c41668bd42d87b36478d72eed60
Ajayy1313 <145823527+Ajayy1313@users.noreply.github.com>
2026-05-14 15:42:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,23 +31,23 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
 
-    ingredients_string += fruit_chosen + ' '
-
-    st.subheader(fruit_chosen + ' Nutrition Information')
-
-    smoothiefroot_response = requests.get(
-        "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
-    )
-
-    if smoothiefroot_response.status_code == 200:
-
-        st.dataframe(
-            data=smoothiefroot_response.json(),
-            use_container_width=True
+        ingredients_string += fruit_chosen + ' '
+    
+        st.subheader(fruit_chosen + ' Nutrition Information')
+    
+        smoothiefroot_response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
         )
-
-    else:
-
-        st.error("The fruit was not found in the SmoothieFroot database.")
+    
+        if smoothiefroot_response.status_code == 200:
+    
+            st.dataframe(
+                data=smoothiefroot_response.json(),
+                use_container_width=True
+            )
+    
+        else:
+    
+            st.error("The fruit was not found in the SmoothieFroot database.")
 
 
